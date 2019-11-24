f9569a87c4a6698d04c6a1822fa4abd7196dc300
arthur makarenko <makarthur914@yahoo.com>
2019-11-24 20:00:15 +0000

    Done.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,6 +1,12 @@
 # Code your solution in calculator.rb
 
+"first_number" = 2
+"second_number" = 3
+
+
 describe "./calculator.rb" do
+
+
 
   it "contains a local variable called first_number that is assigned to a number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
@@ -10,6 +16,8 @@ describe "./calculator.rb" do
 
   it "contains a local variable called second_number that is assigned to a number that isn't 0" do
     second_number = get_variable_from_file('./calculator.rb', "second_number")
+
+
 
     expect(second_number).to be_an(Integer).or be_a(Float)
     expect(second_number).not_to equal(0)
