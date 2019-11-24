a6fc9e7cc8ff7380aa48b8996332e66cb7bdc37a
arthur makarenko <makarthur914@yahoo.com>
2019-11-24 19:36:58 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,6 +2,8 @@
 
 describe "./calculator.rb" do
 
+first_number = 2
+
   it "contains a local variable called first_number that is assigned to a number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
 
